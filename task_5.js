bd8dd10d7549c41dceb2685c548b0e6bb9c2874a
expal <expal123@mail.ru>
2022-03-27 06:01:13 +0300

    hw2

---
+++ task_5.js
@@ -0,0 +1,20 @@
+function adding(a, b) {
+    return a + b
+}
+
+function subtraction(a, b) {
+    return a - b
+}
+
+function multiplication(a, b) {
+    return a * b
+}
+
+function dividing(a, b) {
+    return a / b
+}
+
+console.log(adding(5, 5))
+console.log(subtraction(5, 5))
+console.log(multiplication(5, 5))
+console.log(dividing(5, 5))
---
+++ task_6.js
@@ -0,0 +1,14 @@
+function mathOperation(arg1, arg2, operation) {
+    switch (operation) {
+        case '+':
+                return arg1 + arg2
+        case '-':
+                return arg1 - arg2
+        case '*':
+                return arg1 * arg2
+        case '/':
+                return arg1 / arg2
+    };
+};
+
+console.log(mathOperation(5, 5, "/"))
